7f4d75800837255e1674dd8d315d0f3e4384d5c8
Anthony Johnson <johns9au@mail.uc.edu>
2023-11-21 12:53:12 -0500

    Add project files.

---
+++ Final Projext Group Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Final_Projext_Group_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
