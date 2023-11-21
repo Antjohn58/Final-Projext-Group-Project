65656d0bfeb47c3a2c41318991921d797d735979
Anthony Johnson <johns9au@mail.uc.edu>
2023-11-21 13:50:44 -0500

    l

--- Final Projext Group Project/Models/Team.cs
+++ Final Projext Group Project/Models/Team.cs
@@ -5,8 +5,7 @@
         public int PersonID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public decimal Price { get; set; }
 
-        public string GetDisplayText() => $"{PersonID}  {Name}  {Description} ({Price:c})";
+        public string GetDisplayText() => $"{PersonID}  {Name}  {Description}";
     }
 }
