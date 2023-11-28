208ae0ab2e7787955cf60d83fccccf0afb9d645b
Anthony Johnson <johns9au@mail.uc.edu>
2023-11-28 12:45:31 -0500

    changes

--- Final Projext Group Project/Data/TeamContract.cs
+++ Final Projext Group Project/Data/TeamContract.cs
@@ -1,7 +1,11 @@
-﻿namespace Final_Projext_Group_Project.Data
+﻿using Final_Projext_Group_Project.Models;
+
+namespace Final_Projext_Group_Project.Data
 {
     public interface TeamContract
     {
-
+        int? AddItem(Team i);
+        List<Team> GetAllTeams();
+        Team GetTeamById(int Id);
     }
 }
--- Final Projext Group Project/Data/TeamService.cs
+++ Final Projext Group Project/Data/TeamService.cs
@@ -1,6 +1,53 @@
-﻿namespace Final_Projext_Group_Project.Data
+﻿using Final_Projext_Group_Project.Models;
+
+namespace Final_Projext_Group_Project.Data
 {
-    public class TeamService
+    public class TeamService: TeamContract
     {
+            TeamContext ctx;
+            public TeamService(TeamContext Context)
+            {
+                ctx = Context;
+            }
+
+            public int? AddItem(Team i)
+            {
+                throw new NotImplementedException();
+            }
+
+        public int? AddTeam(Team i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int? AddProduct(Team i)
+            {
+                var product = this.GetTeamById(i.PersonID);
+                if (product != null)
+                {
+                    return null;
+                }
+                ctx.Teams.Add(i);
+                return ctx.SaveChanges();
+            }
+            public List<Team> GetAllTeam()
+            {
+                return ctx.Teams.ToList();
+            }
+
+            public Team GetTeamById(int Id)
+            {
+                return ctx.Teams.FirstOrDefault(x => x.PersonID == Id);
+            }
+
+        List<Team> TeamContract.GetAllTeams()
+        {
+            throw new NotImplementedException();
+        }
+
+        Team TeamContract.GetTeamById(int Id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
