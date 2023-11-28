3572e4ce875881d9550c7beeb6e677546b2d9d13
Alex Brooksbank <brooka4@mail.uc.edu>
2023-11-28 12:49:52 -0500

    Games

---
+++ Final Projext Group Project/Models/Games.cs
@@ -0,0 +1,13 @@
+﻿namespace Final_Projext_Group_Project.Models
+{
+    public class Games
+    {
+        public string PlayerName { get; set; }
+        public string GameTitle { get; set; }
+        public int ReleaseYear{ get; set; }
+        public string Genre { get; set; }
+        public string System { get; set; }
+
+        public string GetDisplayText() => $"{PlayerName}  {GameTitle} {ReleaseYear} {Genre} {System}  ";
+    }
+}
