c9da0aa6ec8aa24303e0cf646311266b5db82db3
Alex Brooksbank <brooka4@mail.uc.edu>
2023-11-30 12:52:34 -0500

    Game Contract

--- Final Projext Group Project/Data/GameContract.cs
+++ Final Projext Group Project/Data/GameContract.cs
@@ -1,6 +1,13 @@
-﻿namespace Final_Projext_Group_Project.Data
+﻿using Final_Projext_Group_Project.Models;
+
+namespace Final_Projext_Group_Project.Data
 {
     public interface GameContract
     {
+        int? AddGame(Games i);
+        List<Games> GetAllGames();
+        Games GetGamesById(int Id);
+        int? RemoveGamesById(int id);
+        int? UpdateGames(Games i);
     }
 }
