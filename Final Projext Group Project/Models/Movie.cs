d8e4e59788e84352a9e461b84b1d2c9d31e294e8
Mckelvin Ofosu-Frimpong <ofosufma@mail.uc.edu>
2023-12-01 14:08:33 -0500

    Mickey's movie commit

---
+++ Final Projext Group Project/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace Final_Projext_Group_Project.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string MovieTitle { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Genre { get; set; }
+        public string Stream { get; set; }
+
+        public string GetDisplayText() => $"{Id}  {MovieTitle} {ReleaseYear} {Genre} {Stream}  ";
+    }
+}
